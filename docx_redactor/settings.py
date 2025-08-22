2e20893d2abe9d1e07856b91d2a83f8b81ff39a6
soumya sagar samal <sagar@soumyas-MacBook-Pro.local>
2025-08-22 15:53:24 +0530

    chore: minor change

--- docx_redactor/settings.py
+++ docx_redactor/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://litera-faker-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://litera-faker-production.up.railway.app']
 
 
 # Application definition
