6621437c27de05165b14bdf8f45abab0372f80d7
soumya sagar samal <sagar@soumyas-MacBook-Pro.local>
2025-08-22 15:50:53 +0530

    chore: debug true

--- docx_redactor/settings.py
+++ docx_redactor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ygfglp+gxzc*^hf1@d*k9#ezm#lrl2tm*lf8i&f0dm2-!9y)@x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = ['*']
